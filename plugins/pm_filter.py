5ce85a9f950378340a26ed234056c5a725906a88
Jithincr7 <99895069+Jithincr7@users.noreply.github.com>
2022-03-02 05:27:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,7 +39,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("സ്വന്തമായി സിനിമ ചോദിക്കു", show_alert=True)
     try:
         offset = int(offset)
     except:
